f4ded77c72c34c34284266f6e8c084330d0ebc88
BloodHaawk <38542796+BloodHaawk@users.noreply.github.com>
2019-04-25 19:24:29 +0100

    Draw a rectangle using two triangles and the Element Buffer Object

--- main.cpp
+++ main.cpp
@@ -52,31 +52,40 @@ int main()
 
 	// Vertex data
 	float vertices[] = {
-		-0.5f,
-		-0.5f,
-		0.0f,
-		0.5f,
-		-0.5f,
-		0.0f,
-		0.0f,
-		0.5f,
-		0.0f};
+		0.5f, 0.5f, 0.0f,   // top right
+		0.5f, -0.5f, 0.0f,  //bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f   // top left
+	};
+
+	// Select vertices to from triangles
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
 
 	// Vertex buffer object
-	unsigned int VBO, VAO;
+	unsigned int VBO, EBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind the VBO
 	glBindVertexArray(0);			  // unbind the VAO
+
+	// Wireframe mode
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
@@ -89,7 +98,8 @@ int main()
 		// Draw the triangle
 		glUseProgram(programID);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices));
+		glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
